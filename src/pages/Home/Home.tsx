e018f2d083058504a37c4e7ab07b2698e5aec8b3
islom-sattorov <slmsttrv1@gmail.com>
2022-12-17 20:21:56 +0500

    rtk query request

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,14 +3,32 @@ import { useGetMoviesQuery } from "../../app/reducers/movie/movieApi";
 import { MovieList } from "../../components/MovieList/MovieList";
 import style from "./Home.module.css";
 
-export const Home = () => {
-  const {
-    data = [],
-    isLoading,
-    isFetching,
-  } = useGetMoviesQuery({ apiKey: movieAPIKEY, s: "Harry", type: "Movie" });
+interface movieData {
+  Poster: string;
+  Title: string;
+  Type: string;
+  Year: string;
+  imdbID: string;
+}
 
-  console.log(data);
+interface apiReq {
+  Response: string;
+  totalResults: string;
+  Search: movieData[];
+}
+
+export const Home = () => {
+  const { data, isLoading, isFetching, isError } = useGetMoviesQuery({
+    apiKey: movieAPIKEY,
+    s: "Harry",
+    type: "Movie",
+  });
+
+  if (isLoading) return <h2>"Loading..."</h2>;
+
+  if (isFetching) return <h2>Get new movies</h2>;
+
+  if (isError) return <h2>Something went wrong</h2>;
 
   return (
     <>
